642b6bb4133483d8808dfc0abae48a5dd2846e4d
Sunil <sunilsv26@gmail.com>
2020-10-10 10:45:06 +0530

    #1 Initial Commit

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const layout = ()=>{
+    return(
+        <Fragment>
+            <div>Tools,Navigation,SideDrawer</div>
+            <div>Burger,Burger Builder</div>
+        </Fragment>
+    )
+}
+
+export default layout
