c4756adb13c916d565845947ebfa2e3a220ec97e
Sunil <sunilsv26@gmail.com>
2020-10-10 12:56:26 +0530

    #11 added dynamic burger component to layout

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
-import classes from './Layout.css'
+import classes from './Layout.css';
+import BurgerBuilder from '../../Container/BurgerBuilder'
 const layout = ()=>{
     return(
         <Fragment>
             <div className={classes.Layout}>Tools,Navigation,SideDrawer</div>
-            <div className={classes.Layout}>Burger,Burger Builder</div>
+            <div className={classes.Layout}><BurgerBuilder/></div>
         </Fragment>
     )
 }
