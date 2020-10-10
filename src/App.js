320d7e72b8e6bcc723fea8097a6e41d3dde3aa3f
Sunil <sunilsv26@gmail.com>
2020-10-10 10:52:23 +0530

    #2 Added Layout Component in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import Layout from './Components/Layout/Layout'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Layout />
       </div>
     );
   }
