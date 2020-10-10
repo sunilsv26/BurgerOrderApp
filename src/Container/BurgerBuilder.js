da525f4fb3ab55b7bb205aac39da04030eac7679
Sunil <sunilsv26@gmail.com>
2020-10-10 13:28:38 +0530

    #13 added ingredients state

--- src/Container/BurgerBuilder.js
+++ src/Container/BurgerBuilder.js
@@ -1,10 +1,18 @@
 import React, { Component, Fragment } from "react";
 import Burger from '../Components/Burger/Burger'
 class BurgerBuilder extends Component {
-  render() {
+    state={
+        ingredients:{
+            meat:1,
+            cheese:1,
+            salad:2,
+            bacon:1
+        }
+    }
+  render() {   
     return (
       <Fragment>
-        <div><Burger /></div>
+        <div><Burger ingredients={this.state.ingredients} /></div>
         <div>Build controls</div>
       </Fragment>
     );
