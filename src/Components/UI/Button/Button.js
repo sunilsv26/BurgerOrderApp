c4a4de7120c6f5e6c8039588f2ed3e84ea96d839
Sunil <sunilsv26@gmail.com>
2020-10-12 15:45:39 +0530

    #40 adjusted array of css classes to render correctly

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import classes from './Button.css'
-const button =(props)=>{
-return <button className={[classes.Button,props.btnType].join(' ')} onClick={props.clicked}>{props.children}</button>
-}
+import React from "react";
+import classes from "./Button.css";
+const button = (props) => {
+  return (
+    <button
+      className={[classes.Button, classes[props.btnType]].join(" ")}
+      onClick={props.clicked}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-export default button
+export default button;
