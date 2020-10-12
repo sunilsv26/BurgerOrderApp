5b589f38984b276a1a25c31ca9a36e8df6e71019
Sunil <sunilsv26@gmail.com>
2020-10-12 15:05:58 +0530

    #34 added bakdrop component to Modal

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Modal.css";
+import Backdrop from '../Backdrop/Backdrop'
 const modal = (props) => {
-  return <div className={classes.Modal} 
-    style={{transform:props.show ? 'translateY(0)':'translateY(-100vh)',
-    opacity:props.show ? '1':'0'}}>
-    {props.children}
-  </div>;
+  return (
+    <Fragment>
+      <Backdrop show={props.show}
+      clicked={props.modalRemoved}/>
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Fragment>
+  );
 };
 
 export default modal;
