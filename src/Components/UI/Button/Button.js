b00248a4d8febb5a58baf400ef246100f24c82c8
Sunil <sunilsv26@gmail.com>
2020-10-12 15:26:15 +0530

    #37 passed onclick property to Btn

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Button.css'
 const button =(props)=>{
-return <button className={[classes.Button,props.btnType].join(' ')}>{props.children}</button>
+return <button className={[classes.Button,props.btnType].join(' ')} onClick={props.clicked}>{props.children}</button>
 }
 
 export default button
