0f6d55a393905c922a409a9d0318579bf37fcfa5
Sunil <sunilsv26@gmail.com>
2020-10-14 12:24:38 +0530

    #46 Rendered toolbar comp and adjusted css props ie margin in layout and burger comp

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.css';
-import BurgerBuilder from '../../Container/BurgerBuilder'
+import BurgerBuilder from '../../Container/BurgerBuilder';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 const layout = ()=>{
     return(
         <Fragment>
-            <div className={classes.Layout}>Tools,Navigation,SideDrawer</div>
+            <div className={classes.Layout}><Toolbar/></div>
             <div className={classes.Layout}><BurgerBuilder/></div>
         </Fragment>
     )
