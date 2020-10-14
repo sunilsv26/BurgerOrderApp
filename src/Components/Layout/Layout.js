1c0c120e8f72c5c716f0269b24bd4ccbdc2cf137
Sunil <sunilsv26@gmail.com>
2020-10-14 15:16:47 +0530

    #56 sideDrawer comp imported  and rendered in layout comp

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.css';
 import BurgerBuilder from '../../Container/BurgerBuilder';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer  from '../Navigation/SideDrawer/SideDrawer'
+
 const layout = ()=>{
     return(
         <Fragment>
             <div className={classes.Layout}><Toolbar/></div>
+            <SideDrawer />
             <div className={classes.Layout}><BurgerBuilder/></div>
         </Fragment>
     )
