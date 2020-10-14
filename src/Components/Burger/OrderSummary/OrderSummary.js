672936db06d7844b0b3457cdc53ab8980789721c
Sunil <sunilsv26@gmail.com>
2020-10-14 11:41:37 +0530

    #42 Added total price to orderSummary

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,21 +1,33 @@
-import React, { Fragment } from 'react';
-import Button from '../../UI/Button/Button'
+import React, { Fragment } from "react";
+import Button from "../../UI/Button/Button";
 
-const orderSummary =(props)=>{
-const ingredientsOrderSummary= Object.keys(props.ingredients).map((igKey)=>{return (<li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}</li>) })
+const orderSummary = (props) => {
+  const ingredientsOrderSummary = Object.keys(props.ingredients).map(
+    (igKey) => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+          {props.ingredients[igKey]}
+        </li>
+      );
+    }
+  );
 
-    return(
-        <Fragment>
-            <h3>Your Order Summary</h3>
-            <p>Here is your delecious Burger with following Ingredients:</p>
-            <ul>
-             {ingredientsOrderSummary}
-            </ul>
-            <p>Continue to Checkout!</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>Contiue</Button>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <h3>Your Order Summary</h3>
+      <p>Here is your delecious Burger with following Ingredients:</p>
+      <ul>{ingredientsOrderSummary}</ul>
+      <strong>Total Price : {props.price}</strong>
+      <p>Continue to Checkout!</p>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        Contiue
+      </Button>
+    </Fragment>
+  );
+};
 
-export default orderSummary
+export default orderSummary;
