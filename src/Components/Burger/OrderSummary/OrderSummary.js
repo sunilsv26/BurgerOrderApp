8ba35af5d263e154401ab4198705d9c736226a18
Sunil <sunilsv26@gmail.com>
2020-10-15 12:47:44 +0530

    #70 Changed to class base comp to analyse

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientsOrderSummary = Object.keys(props.ingredients).map(
-    (igKey) => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-          {props.ingredients[igKey]}
-        </li>
-      );
-    }
-  );
-
-  return (
-    <Fragment>
-      <h3>Your Order Summary</h3>
-      <p>Here is your delecious Burger with following Ingredients:</p>
-      <ul>{ingredientsOrderSummary}</ul>
-      <strong>Total Price : {props.price}</strong>
-      <p>Continue to Checkout!</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>
-        Cancel
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        Contiue
-      </Button>
-    </Fragment>
-  );
+class OrderSummary extends Component {
+  render(){
+    const ingredientsOrderSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+    return (
+      <Fragment>
+        <h3>Your Order Summary</h3>
+        <p>Here is your delecious Burger with following Ingredients:</p>
+        <ul>{ingredientsOrderSummary}</ul>
+        <strong>Total Price : {this.props.price}</strong>
+        <p>Continue to Checkout!</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          Contiue
+        </Button>
+      </Fragment>
+    );
+    
+  } 
 };
 
-export default orderSummary;
+export default OrderSummary;
