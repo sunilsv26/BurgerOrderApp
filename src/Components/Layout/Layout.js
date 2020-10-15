9a5160f95ba1ca2c2232613c4e1f189cfd6bc200
Sunil <sunilsv26@gmail.com>
2020-10-15 10:51:16 +0530

    #60 Changed layout comp from functional to statful class

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,17 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Layout.css';
 import BurgerBuilder from '../../Container/BurgerBuilder';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer  from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = ()=>{
-    return(
-        <Fragment>
+class Layout extends Component{
+    render(){
+        return( <Fragment>
             <div className={classes.Layout}><Toolbar/></div>
             <SideDrawer />
             <div className={classes.Layout}><BurgerBuilder/></div>
-        </Fragment>
-    )
+        </Fragment>)
+    }
 }
 
-export default layout
+export default Layout
