a0a338fe42b6d2cb9245abc121af65527a212193
Sunil <sunilsv26@gmail.com>
2020-10-15 12:06:04 +0530

    #63 Added sideDrawerToggle method

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,24 +1,39 @@
-import React, { Component, Fragment } from 'react';
-import classes from './Layout.css';
-import BurgerBuilder from '../../Container/BurgerBuilder';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer  from '../Navigation/SideDrawer/SideDrawer'
+import React, { Component, Fragment } from "react";
+import classes from "./Layout.css";
+import BurgerBuilder from "../../Container/BurgerBuilder";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-class Layout extends Component{
-    state={
-        showSideDrawer:true,
-    }
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-    sideDrawerCancelHandler=()=>{
-        this.setState({showSideDrawer:false})
-    }
-    render(){
-        return( <Fragment>
-            <div className={classes.Layout}><Toolbar/></div>
-            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCancelHandler}/>
-            <div className={classes.Layout}><BurgerBuilder/></div>
-        </Fragment>)
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  sideDrawerCancelHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.Layout}>
+          <Toolbar  sideDrawerToggler={this.sideDrawerToggleHandler} />
+        </div>
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCancelHandler}
+        />
+        <div className={classes.Layout}>
+          <BurgerBuilder />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
