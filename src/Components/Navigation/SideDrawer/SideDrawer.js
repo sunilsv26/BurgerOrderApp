c400fd6af526999e88a89b09910bc53b4a804a06
Sunil <sunilsv26@gmail.com>
2020-10-15 11:13:19 +0530

    #62 Added dynamic class to sideDrawer comp

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -5,10 +5,14 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const sideDrawer = (props) => {
+  let sideDrawerClasses = [classes.SideDrawer,classes.Close];
+  if(props.open){
+    sideDrawerClasses=[classes.SideDrawer,classes.Open]
+  }
   return (
     <Fragment>
       <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={classes.SideDrawer}>
+      <div className={sideDrawerClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo />
         </div>
