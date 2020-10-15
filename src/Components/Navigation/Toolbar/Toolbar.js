549a78fcccf432d4d89ad3e23c8f2ae1c41beab6
Sunil <sunilsv26@gmail.com>
2020-10-15 12:13:43 +0530

    #66 removed nav items from toolbar in mobile mode

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Toolbar.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import SideDrawerToggler from "../../Navigation/SideDrawer/DrawerToggle/DrawerToggle";
 
-const toolbar =(props)=>{
-    return(
-        <header className={classes.Toolbar}>
-            <div>Menu</div>
-            <NavigationItems />
-            <Logo />
-        </header>
-    )
-}
+const toolbar = (props) => {
+  return (
+    <header className={classes.Toolbar}>
+      <SideDrawerToggler clicked={props.sideDrawerToggler} />
+      <Logo />
+      <div className={classes.DesktopOnly}>
+      <NavigationItems />
+      </div>
+      
+    </header>
+  );
+};
 
-export default toolbar
+export default toolbar;
