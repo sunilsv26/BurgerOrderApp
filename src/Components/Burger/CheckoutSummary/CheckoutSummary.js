492cb4efa39ed53ae58fc9d33ca0f7cbaf2b4b05
Sunil <sunilsv26@gmail.com>
2020-10-25 11:55:11 +0530

    #82 Initialized checkout,checkoutSummary componenent

---
+++ src/Components/Burger/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  classes from './CheckoutSummary.classes';
+
+const checkoutSummmary =(props)=>{
+    return(
+        <div className={classes.CheckoutSummary} >
+        </div>
+    )
+}
