343c0eb0990fd30bd1c8ab0f1fa1f133dc04308f
Sunil <sunilsv26@gmail.com>
2020-10-25 12:55:35 +0530

    #88 added routes to burger and chkout page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import Layout from "./Components/Layout/Layout";
 import Checkout from "./Container/Checkout";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route ,Redirect} from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <Layout />
-          <Checkout />
+          <Route path="/checkout" exact component={Checkout} />
+          <Route path="/burger" exact component={Layout} />
+          <Route path="/" exact component={Layout} />
         </BrowserRouter>
       </div>
     );
