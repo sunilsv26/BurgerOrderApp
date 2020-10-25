a8e486b739a4ad6720aa72ec5483afd110a3a385
Sunil <sunilsv26@gmail.com>
2020-10-25 13:47:22 +0530

    #91 adjusted layout comp to rnder nav bar in both burger and chkout

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from "react";
 import Layout from "./Components/Layout/Layout";
 import Checkout from "./Container/Checkout";
 import "./App.css";
-import { BrowserRouter, Route ,Redirect} from "react-router-dom";
+import BurgerBuilder from "./Container/BurgerBuilder";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <Route path="/checkout" exact component={Checkout} />
-          <Route path="/burger" exact component={Layout} />
-          <Route path="/" exact component={Layout} />
+          <Layout>
+            <Route path="/checkout" exact component={Checkout} />
+            <Route path="/burger" exact component={BurgerBuilder} />
+          </Layout>
         </BrowserRouter>
       </div>
     );
