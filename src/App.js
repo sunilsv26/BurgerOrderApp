a505ea48c0fa9ccc23c18b3cfd1171a6795f83c5
Sunil <sunilsv26@gmail.com>
2020-10-25 12:35:23 +0530

    #87 wrapped app comp in browserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import Layout from './Components/Layout/Layout';
-import Checkout from './Container/Checkout'
-import './App.css';
-
+import React, { Component } from "react";
+import Layout from "./Components/Layout/Layout";
+import Checkout from "./Container/Checkout";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Layout />
-        <Checkout />
+        <BrowserRouter>
+          <Layout />
+          <Checkout />
+        </BrowserRouter>
       </div>
     );
   }
