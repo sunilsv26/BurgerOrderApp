ee270fa08c6a857a9fdf0680c72684863ea7aca4
Sunil <sunilsv26@gmail.com>
2020-10-25 13:48:49 +0530

    #92 wrapped burger in div with styles

--- src/Components/Burger/CheckoutSummary/CheckoutSummary.js
+++ src/Components/Burger/CheckoutSummary/CheckoutSummary.js
@@ -3,10 +3,11 @@ import  classes from './CheckoutSummary.css';
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button'
 const checkoutSummmary =(props)=>{
+    
     return(
         <div className={classes.CheckoutSummary} >
             <h1>Hope you will like the taste of it!!!</h1>
-            <div style={{width:'auto',margin:'auto'}}>
+            <div className={classes.Burger}>
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button btnType='Danger' clicked>CANCEL</Button>
