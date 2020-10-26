c37e86eb1bd87c9a060479e6000d0aa97e197d3a
Sunil <sunilsv26@gmail.com>
2020-10-26 16:06:19 +0530

    #106 Added new container orders

---
+++ src/Container/Orders.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Order from "../Components/Burger/OrderSummary/Order/Order";
+
+class Orders extends Component {
+  render() {
+    return (
+      <div>
+        <Order />
+      </div>
+    );
+  }
+}
+
+export default Orders
