62fcb81138579c7dbe01ca8512cebbc26f7dc0e0
Sunil <sunilsv26@gmail.com>
2020-10-26 16:37:13 +0530

    #111 Fetching data from database

--- src/Container/Orders.js
+++ src/Container/Orders.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import Order from "../Components/Burger/OrderSummary/Order/Order";
+import axios from '../axios-order'
 
 class Orders extends Component {
+  state ={
+    orders:[],
+    loading:true,
+  }
+  componentDidMount(){
+    axios.get('/orders.jason')
+    .then(res=>{
+      let resData = [];
+      for (let key in res.data){
+        resData.push({...resData[key],id:key})
+      }
+      this.setState({orders:resData,loading:false})
+    }).catch(err=>this.setState({loading:false}))
+  }
   render() {
     return (
       <div style={{marginTop:'55px'}}>
