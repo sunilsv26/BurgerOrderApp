ee0d0bed0049aa9a8c12057c046d7d504d5212ec
Sunil <sunilsv26@gmail.com>
2020-10-31 16:20:31 +0530

    #137 imported Provider,reducer,creatStore from redux,reactRedux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route,Redirect } from "react-router-dom";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+ 
 import Layout from "./Components/Layout/Layout";
 import Checkout from "./Container/Checkout";
 import "./App.css";
 import BurgerBuilder from "./Container/BurgerBuilder";
-import { BrowserRouter, Route,Redirect } from "react-router-dom";
-import Orders from './Container/Orders'
+import reducer from './store/reducer'
+import Orders from './Container/Orders';
+
+const store = createStore(reducer)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Provider store= {store}>
         <BrowserRouter>
           <Layout>
             <Route path="/checkout"  component={Checkout} />
@@ -18,6 +25,7 @@ class App extends Component {
             <Redirect to='/burger'/>
            </Layout>
         </BrowserRouter>
+        </Provider>
       </div>
     );
   }
